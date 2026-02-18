c9949941ffcf8f632bee167b8828141ee32d8966
PoshikaM <poshika.m@kalvium.community>
2026-02-18 22:14:11 +0530

    frontend angular setup

---
+++ task-manager-ui/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+// import { Routes } from '@angular/router';
+
+// export const routes: Routes = [];
+
+import { Routes } from '@angular/router';
+import { TasksComponent } from './tasks/tasks';
+
+export const routes: Routes = [
+  { path: '', component: TasksComponent }
+];
---
+++ task-manager-ui/src/app/app.ts
@@ -0,0 +1,23 @@
+// import { Component, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.html',
+//   styleUrl: './app.css'
+// })
+// export class App {
+//   protected readonly title = signal('task-manager-ui');
+// }
+
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`
+})
+export class App {}
